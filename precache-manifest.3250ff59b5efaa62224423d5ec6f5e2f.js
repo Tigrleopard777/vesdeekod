e2539a5e5ca3aca35b9b5d72028f9983c0a7b104
Ruslan <ryslanvlasov@mail.ru>
2020-09-15 02:59:34 +0300

    first

---
+++ precache-manifest.3250ff59b5efaa62224423d5ec6f5e2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8a2fb03a41602855ec5c022f31568a",
+    "url": "/vesdeekod/index.html"
+  },
+  {
+    "revision": "49e68096f94bf7f3a994",
+    "url": "/vesdeekod/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "49e68096f94bf7f3a994",
+    "url": "/vesdeekod/static/js/2.dcd3563a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vesdeekod/static/js/2.dcd3563a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336305b78fc9a79f9223",
+    "url": "/vesdeekod/static/js/main.0d6391de.chunk.js"
+  },
+  {
+    "revision": "4f25ccb27f8f044ec3e9",
+    "url": "/vesdeekod/static/js/runtime-main.dc63fe26.js"
+  }
+]);
